0bffd738004b4df93662ce906c262f032eb73418
Facundo Montiel <montiel.facu@gmail.com>
2022-07-21 18:18:17 -0300

    Ejercicio 6

---
+++ js/ejercicio6.js
@@ -0,0 +1,6 @@
+for(let indicePiramide = 1; indicePiramide<31; indicePiramide++){
+    for(let cantidadDeNumeros = 1; cantidadDeNumeros<=indicePiramide; cantidadDeNumeros++){
+        document.write(indicePiramide);
+    }
+    document.write("<br>");
+}
