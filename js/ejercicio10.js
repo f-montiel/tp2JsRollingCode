406e83645d7490ff0be28752e5a8eb85cbc85c54
Facundo Montiel <montiel.facu@gmail.com>
2022-07-21 19:14:26 -0300

    ejercicio 10

---
+++ js/ejercicio10.js
@@ -0,0 +1,15 @@
+let filas = parseInt(prompt("Ingrese el numero de filas"));
+let columnas = parseInt(prompt("Ingrese el numero de columnas"));
+let cantidadCeldas = filas * columnas;
+document.write("<table><tbody>");
+for(let indiceFilas = 0; indiceFilas < filas; indiceFilas++){
+    document.write("<tr>");
+    for(let indiceColumnas = 0; indiceColumnas < columnas; indiceColumnas++){
+        document.write("<td>");
+        document.write(cantidadCeldas);
+        cantidadCeldas--;
+        document.write("</td>");
+    }
+    document.write("</tr>");
+}
+document.write("</tbody></table>");
